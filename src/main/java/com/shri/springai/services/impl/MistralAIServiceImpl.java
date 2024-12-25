3c187c96727e6f3c057bf62e41e97d71bf554109
zeeroiq <shrikantvishwakarma@outlook.com>
2024-12-25 23:08:57 +0530

    directing LLM to respond in JSON. closes #4

--- src/main/java/com/shri/springai/services/impl/MistralAIServiceImpl.java
+++ src/main/java/com/shri/springai/services/impl/MistralAIServiceImpl.java
@@ -1,9 +1,13 @@
 package com.shri.springai.services.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shri.springai.records.Answer;
 import com.shri.springai.records.GetCapitalRequest;
 import com.shri.springai.records.Question;
 import com.shri.springai.services.AIService;
+import org.slf4j.Logger;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
@@ -18,8 +22,11 @@ import java.util.Map;
 @Service(value = "mistralAIService")
 public class MistralAIServiceImpl implements AIService {
 
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(MistralAIServiceImpl.class);
+
     private final ChatClient.Builder chatClient;
     private final ChatModel chatModel;
+    private final ObjectMapper jacksonObjectMapper;
 
     @Value("classpath:templates/get_capital_prompt.st")
     private Resource getCapitalPromptTemplate;
@@ -28,9 +35,10 @@ public class MistralAIServiceImpl implements AIService {
     private Resource getGetCapitalInfoPromptTemplate;
 
 
-    public MistralAIServiceImpl(ChatClient.Builder chatClient, ChatModel chatModel) {
+    public MistralAIServiceImpl(ChatClient.Builder chatClient, ChatModel chatModel, ObjectMapper jacksonObjectMapper) {
         this.chatClient = chatClient;
         this.chatModel = chatModel;
+        this.jacksonObjectMapper = jacksonObjectMapper;
     }
 
     @Override
@@ -61,8 +69,17 @@ public class MistralAIServiceImpl implements AIService {
     public Answer getCapital(GetCapitalRequest getCapitalRequest) {
         PromptTemplate promptTemplate = new PromptTemplate(getCapitalPromptTemplate);
         Prompt prompt = promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry()));
-        ChatResponse call = chatModel.call(prompt);
-        return new Answer(call.getResult().getOutput().getContent());
+        ChatResponse response = chatModel.call(prompt);
+        String content = response.getResult().getOutput().getContent();
+        log.info("Response: {}", content);
+        String responseAsString;
+        try {
+            JsonNode jsonNode = jacksonObjectMapper.readTree(content);
+            responseAsString = jsonNode.get("answer").asText();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return new Answer(responseAsString);
     }
 
     @Override
